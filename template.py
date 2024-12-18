615cafccfb74971d4a9c6321e24124983561ab8e
linda_lenkens <linda.lenkens@datasciencelab.nl>
2024-12-18 12:19:41 +0100

    folder structure

---
+++ template.py
@@ -0,0 +1,55 @@
+# to get a structure helps not manually create structure
+
+# always needed
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "mlProject"
+
+# the folders and files needed for projects
+list_of_files = [
+    ".github/workflows/.gitkeep", #foldername
+    f"src/{project_name}/__init__.py" #files
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb", # because we experiment in notebooks
+    "templates/index.html"
+]
+
+
+# loop to create the above folders and files
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+# create folders
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+# create files    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+        
+    else:
+        logging.info(f"{filename} already exists")
