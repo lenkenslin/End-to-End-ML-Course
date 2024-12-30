4e2c0f593d333df03f75c6522f7df72ebafcacb6
linda_lenkens <linda.lenkens@datasciencelab.nl>
2024-12-30 13:51:32 +0100

    check

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -1,4 +1,4 @@
-import OS
+import os
 from box.exceptions import BoxValueError
 import yaml
 import json
@@ -7,3 +7,48 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns"""
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories"""
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """save json data"""
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data:Any, path: Path):
+    joblib.dump(value=data, filename=path)
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    data = joblib.load(path)
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
